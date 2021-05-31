6e228c474d65b396a2e5ef4cc52e6192a34d0b34
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2021-05-30 19:34:34 -0500

    Update math.h

--- C/include/math.h
+++ C/include/math.h
@@ -2,3 +2,22 @@
 #include <stddef.h>
 #include <stdint.h>
 
+double exp(double x){
+	
+}
+
+double pow(double x, uint16_t y){
+	double s = x;
+	while(y-->0){
+		x *= s;
+	}
+	return s;
+}
+
+unsigned long long fac(uint8_t x){
+	unsigned long long y = 1;
+	while(x-->0){
+		y *= x;
+	}
+	return y;
+}
