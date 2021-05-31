c1f5d736b9a6bfe0c18de12e684effb4d4d09cad
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2021-05-31 13:23:56 -0500

    Update stdlib.h

--- C/include/stdlib.h
+++ C/include/stdlib.h
@@ -55,7 +55,53 @@ int atoi(const char *str)
     return acc;
 }
 
+long int
+atol(string)
+    register char *string;	/* String of ASCII digits, possibly
+				 * preceded by white space.  For bases
+				 * greater than 10, either lower- or
+				 * upper-case digits may be used.
+				 */
+{
+    register long int result = 0;
+    register unsigned int digit;
+    int sign;
 
+    /*
+     * Skip any leading blanks.
+     */
+
+    while (*string == ' ') {
+	string += 1;
+    }
+
+    /*
+     * Check for a sign.
+     */
+
+    if (*string == '-') {
+	sign = 1;
+	string += 1;
+    } else {
+	sign = 0;
+	if (*string == '+') {
+	    string += 1;
+	}
+    }
+
+    for ( ; ; string += 1) {
+	digit = *string - '0';
+	if (digit > 9) {
+	    break;
+	}
+	result = (10*result) + digit;
+    }
+
+    if (sign) {
+	return -result;
+    }
+    return result;
+}
 
 
 
