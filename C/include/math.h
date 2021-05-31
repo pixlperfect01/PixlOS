7216fc282313a256d8bff5b6c0eb5eda8fdc8768
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2021-05-30 23:09:20 -0500

    Update math.h

--- C/include/math.h
+++ C/include/math.h
@@ -3,7 +3,23 @@
 #include <stdint.h>
 
 double exp(double x){
-	
+	// e^0 = 1 (duh)
+	// e^0.5 = 1.6487212707
+	// e^1 = 2.718281828459 (also duh)
+	// lin interp:  y = y1 + ((x – x1) / (x2 – x1)) * (y2 – y1)
+	uint8_t ipart = floor(x + 0.5);
+	double fpart = x - ipart;
+	double oupt = pow(2.718281828459, ipart);
+	if(fpart > 0.5)
+	{
+		oupt *= 1.6487212707 + ((fpart - 0.5) / ( 0.5)) * (1.06956056);
+	} else if(fpart < 0.5)
+	{
+		oupt *= 1 + (fpart / 0.5 * 0.6487212707);
+	} else {
+		oupt *= 1.6487212707;
+	}
+	return oupt;
 }
 
 double pow(double x, uint16_t y){
@@ -11,7 +27,11 @@ double pow(double x, uint16_t y){
 	while(y-->0){
 		x *= s;
 	}
-	return s;
+	return x;
+}
+
+double powf(double x, double y){
+	
 }
 
 unsigned long long fac(uint8_t x){
@@ -20,4 +40,8 @@ unsigned long long fac(uint8_t x){
 		y *= x;
 	}
 	return y;
+}
+
+double ln(double x){
+	
 }
