db7cb7183dbe909c1ceabff2a22a21045f808144
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2021-06-01 09:11:31 -0500

    Update math.c

--- include/math.c
+++ include/math.c
@@ -1,3 +1,13 @@
 #include <stdint.h>
 
 #define PI 3.14159265358979323846
+
+double acos(double x){
+// 	acos(x) ≈ π/2 + (ax + bx³) / (1 + cx² + dx⁴)
+	double x2 = x*x;
+	return PI/2 + (-0.939115566365855+0.9217841528914573*x2*x) / (-1.2845906244690837*x2 + 0.295624144969963174*x2*x2 + 1);
+}
+
+double asin(double x){
+	return PI/2 - acos(x);
+}
